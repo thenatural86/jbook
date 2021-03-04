b3d614633f2b4806d1e0cbcab9f5e2c18be16aa3
thenatural86 <philip.smalls86@gmail.com>
2021-03-04 16:39:29 -0500

    bundle plugin

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as esbuild from 'esbuild-wasm'
 import ReactDOM from 'react-dom'
 import { useState, useEffect, useRef } from 'react'
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
 
 const App = () => {
   const ref = useRef<any>()
@@ -24,12 +25,14 @@ const App = () => {
       return
     }
     // console.log(ref.current)
-    const result = await ref.current.transform(input, {
-      loader: 'jsx',
-      target: 'es2015',
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
     })
-    // console.log(result)
-    setCode(result.code)
+    console.log(result)
+    setCode(result.outputFiles[0].text)
   }
 
   return (
