c3b514de9ea0c0bc7507677932e40eecbcc369c7
thenatural86 <philip.smalls86@gmail.com>
2021-03-12 15:52:51 -0500

    code formattng on click

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,6 +1,7 @@
 import MonacoEditor, { EditorDidMount } from '@monaco-editor/react'
 import prettier from 'prettier'
 import parser from 'prettier/parser-babel'
+import { useRef } from 'react'
 
 interface CodeEditorProps {
   initialValue: string
@@ -8,7 +9,10 @@ interface CodeEditorProps {
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
+  const editorRef = useRef<any>()
+
   const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+    editorRef.current = monacoEditor
     monacoEditor.onDidChangeModelContent(() => {
       onChange(getValue())
     })
@@ -17,8 +21,18 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
 
   const onFormatClick = () => {
     //get current value from editor
+    const unformatted = editorRef.current.getModel().getValue()
+
     //format that value
+    const formatted = prettier.format(unformatted, {
+      parser: 'babel',
+      plugins: [parser],
+      useTabs: false,
+      semi: true,
+      singleQuote: true,
+    })
     //set the formatted value back in the editor
+    editorRef.current.setValue(formatted)
   }
 
   return (
