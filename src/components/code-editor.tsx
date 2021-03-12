384ef159e06dc687ca67c8205921c9d5ad64d30f
thenatural86 <philip.smalls86@gmail.com>
2021-03-12 10:20:32 -0500

    base editor config

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,7 +1,7 @@
 import MonacoEditor from '@monaco-editor/react'
 
 const CodeEditor = () => {
-  return <MonacoEditor height='500px' />
+  return <MonacoEditor language='javascript' height='200px' />
 }
 
 export default CodeEditor
