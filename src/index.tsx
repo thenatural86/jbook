4d1595a1b5d3f9e3edf4e2cc20fb180aa94dde73
thenatural86 <philip.smalls86@gmail.com>
2021-03-13 12:23:40 -0500

    making a editor component

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,12 @@
 import ReactDOM from 'react-dom'
-import { useState } from 'react'
-import CodeEditor from './components/code-editor'
 import 'bulmaswatch/superhero/bulmaswatch.min.css'
-import Preview from './components/preview'
-import bundle from './bundler'
+import CodeCell from './components/code-cell'
 
 const App = () => {
-  const [input, setInput] = useState('')
-  const [code, setCode] = useState('')
-
-  const onClick = async () => {
-    const output = await bundle(input)
-    setCode(output)
-  }
-
   return (
     <div>
-      <CodeEditor
-        initialValue='const yolo = "you only live once, imma bet it all"'
-        onChange={(value) => setInput(value)}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <Preview code={code} />
+      <CodeCell />
+      <CodeCell />
     </div>
   )
 }
