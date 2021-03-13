cabdc80598f53d0e6419951d40fe42df7efb6a9a
thenatural86 <philip.smalls86@gmail.com>
2021-03-13 11:38:06 -0500

    refactoring of preview component

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,12 @@ import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
 import { fetchPlugin } from './plugins/fetch-plugin'
 import CodeEditor from './components/code-editor'
 import 'bulmaswatch/superhero/bulmaswatch.min.css'
+import Preview from './components/preview'
 
 const App = () => {
   const ref = useRef<any>()
-  const iframe = useRef<any>()
   const [input, setInput] = useState('')
+  const [code, setCode] = useState('')
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -27,8 +28,6 @@ const App = () => {
       return
     }
 
-    iframe.current.srcdoc = html
-
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -39,29 +38,8 @@ const App = () => {
         global: 'window',
       },
     })
-    // setCode(result.outputFiles[0].text)
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
+    setCode(result.outputFiles[0].text)
   }
-
-  const html = `
-    <html>
-      <head></head>
-      <body>
-        <div id="root"></div>
-        <script>
-          window.addEventListener('message', (event) => {
-            try {
-              eval(event.data)
-            } catch (err){
-              const root = document.querySelector('#root')
-              root.innerHTML = '<div style="color:red"><h4>Runtime Error</h4>' + err + '<div>'
-              console.error(err)
-            } 
-          }, false)
-        </script>
-      </body>
-    </html>
-  `
 
   return (
     <div>
@@ -69,20 +47,10 @@ const App = () => {
         initialValue='const yolo = "you only live once, imma bet it all"'
         onChange={(value) => setInput(value)}
       />
-      <textarea
-        style={{ height: '10rem', width: '20rem' }}
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <iframe
-        title='preview'
-        ref={iframe}
-        sandbox='allow-scripts'
-        srcDoc={html}
-      />
+      <Preview code={code} />
     </div>
   )
 }
