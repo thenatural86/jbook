70a7a3428fe0340abe331fc4d6382a5c03a7564b
thenatural86 <philip.smalls86@gmail.com>
2021-03-14 11:41:48 -0400

    resizing preview window

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -10,6 +10,7 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
 
   if (direction === 'horizontal') {
     resizsableProps = {
+      className: 'resize-horizontal',
       minConstraints: [window.innerWidth * 0.2, Infinity],
       maxConstraints: [window.innerWidth * 0.75, Infinity],
       height: Infinity,
