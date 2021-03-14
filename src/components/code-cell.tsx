1298d0dbab3807092b9c077d073cbc7441f3c70e
thenatural86 <philip.smalls86@gmail.com>
2021-03-14 16:08:25 -0400

    print error

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -30,7 +30,7 @@ const CodeCell = () => {
             onChange={(value) => setInput(value)}
           />
         </Resizable>
-        <Preview code={code} />
+        <Preview code={code} err={err} />
       </div>
     </Resizable>
   )
