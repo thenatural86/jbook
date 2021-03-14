003af863b9ea32761d9064ec19a05033ca7c9d95
thenatural86 <philip.smalls86@gmail.com>
2021-03-14 12:48:20 -0400

    setTimeout added for bundling code

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import CodeEditor from './code-editor'
 import Preview from './preview'
 import bundle from '../bundler'
@@ -8,10 +8,12 @@ const CodeCell = () => {
   const [input, setInput] = useState('')
   const [code, setCode] = useState('')
 
-  const onClick = async () => {
-    const output = await bundle(input)
-    setCode(output)
-  }
+  useEffect(() => {
+    setTimeout(async () => {
+      const output = await bundle(input)
+      setCode(output)
+    }, 1000)
+  }, [input])
 
   return (
     <Resizable direction='vertical'>
