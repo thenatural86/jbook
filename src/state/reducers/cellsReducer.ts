c71c6087f95f397332f8a20d294342ece5300b99
thenatural86 <philip.smalls86@gmail.com>
2021-03-17 19:49:26 -0400

    adding reducer cases

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -18,7 +18,21 @@ const initialState: CellsState = {
   data: {},
 }
 
-const reducer = (state: CellsState, action: Action): CellsState => {
-  return state
+const reducer = (
+  state: CellsState = initialState,
+  action: Action
+): CellsState => {
+  switch (action.type) {
+    case ActionType.UPDATE_CELL:
+      return state
+    case ActionType.DELETE_CELL:
+      return state
+    case ActionType.MOVE_CELL:
+      return state
+    case ActionType.INSERT_CELL_BEFORE:
+      return state
+    default:
+      return state
+  }
 }
 export default reducer
