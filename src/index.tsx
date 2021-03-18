cacb3216e9942f50833911092a44e9e6fd4f7a3e
thenatural86 <philip.smalls86@gmail.com>
2021-03-18 18:30:57 -0400

    connecting react to redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { store } from './state'
+
 import 'bulmaswatch/superhero/bulmaswatch.min.css'
 import CodeCell from './components/code-cell'
 import TextEditor from './components/text-editor'
 
 const App = () => {
   return (
-    <div>
-      <CodeCell />
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div>
+        <CodeCell />
+        <TextEditor />
+      </div>
+    </Provider>
   )
 }
 
