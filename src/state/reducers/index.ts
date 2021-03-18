6a30a68b81f44284a5c2f1c65163246287ff3b45
thenatural86 <philip.smalls86@gmail.com>
2021-03-18 18:27:18 -0400

    creating and exporting the store

--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -1,1 +1,10 @@
-export {}
+import cellsReducer from './cellsReducer'
+import { combineReducers } from 'redux'
+
+const reducers = combineReducers({
+  cells: cellsReducer,
+})
+
+export default reducers
+
+export type RootState = ReturnType<typeof reducers>
