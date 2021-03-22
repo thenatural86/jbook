751113d4353d2f745a617d42457c5057fd9c0db6
thenatural86 <philip.smalls86@gmail.com>
2021-03-22 16:03:37 -0400

    rendering a list of cells and imorting Cell type to cell list item

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,4 +1,5 @@
 import { useTypedSelector } from '../hooks/use-typed-selector'
+import CellListItem from './cell-list-item'
 
 const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) => {
@@ -7,7 +8,11 @@ const CellList: React.FC = () => {
     })
   })
 
-  return <div>Cell List</div>
+  const renderedCells = cells.map((cell) => (
+    <CellListItem key={cell.id} cell={cell} />
+  ))
+
+  return <div>{renderedCells}</div>
 }
 
 export default CellList
