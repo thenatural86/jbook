1bbfce0ce5a817cba09a634fc0ca6f2397ef6d0d
thenatural86 <philip.smalls86@gmail.com>
2021-03-22 15:38:18 -0400

    render cell list

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,17 @@ import { Provider } from 'react-redux'
 import { store } from './state'
 
 import 'bulmaswatch/superhero/bulmaswatch.min.css'
-import CodeCell from './components/code-cell'
-import TextEditor from './components/text-editor'
+// import CodeCell from './components/code-cell'
+// import TextEditor from './components/text-editor'
+import CellList from './components/cell-list'
 
 const App = () => {
   return (
     <Provider store={store}>
       <div>
-        <CodeCell />
-        <TextEditor />
+        <CellList />
+        {/* <CodeCell />
+        <TextEditor /> */}
       </div>
     </Provider>
   )
