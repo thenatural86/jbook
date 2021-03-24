75516c3c7678a03aed54c94ea1a47cfa2e361de0
thenatural86 <philip.smalls86@gmail.com>
2021-03-24 19:54:28 -0400

    cell visibility issue fix

--- src/components/add-cell.tsx
+++ src/components/add-cell.tsx
@@ -4,14 +4,15 @@ import { useActions } from '../hooks/use-actions'
 
 interface AddCellProps {
   nextCellId: string | null
+  forceVisible?: boolean
   // cellType: CellTypes
 }
 
-const AddCell: React.FC<AddCellProps> = ({ nextCellId }) => {
+const AddCell: React.FC<AddCellProps> = ({ forceVisible, nextCellId }) => {
   const { insertCellBefore } = useActions()
 
   return (
-    <div className='add-cell'>
+    <div className={`add-cell ${forceVisible && 'force-visible'}`}>
       <div className='add-buttons'>
         <button
           className='button is-rounded is-primary is-small'
