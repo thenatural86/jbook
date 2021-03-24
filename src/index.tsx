71cfc638debc7edf39b7936fd4e2e7a68f4b7e75
thenatural86 <philip.smalls86@gmail.com>
2021-03-24 12:08:42 -0400

    adding in font awesome css

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { store } from './state'
+import CellList from './components/cell-list'
 
+import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'bulmaswatch/superhero/bulmaswatch.min.css'
+
 // import CodeCell from './components/code-cell'
 // import TextEditor from './components/text-editor'
-import CellList from './components/cell-list'
 
 const App = () => {
   return (
