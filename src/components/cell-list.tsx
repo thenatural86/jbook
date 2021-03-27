4dcde6b2dcecb374597d3142a5d60fe1b1107d0a
thenatural86 <philip.smalls86@gmail.com>
2021-03-27 13:30:38 -0400

    css fix for bottom margin of last cell

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -3,6 +3,8 @@ import CellListItem from './cell-list-item'
 import AddCell from './add-cell'
 import { Fragment } from 'react'
 
+import './cell-list.css'
+
 const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) =>
     order.map((id) => data[id])
@@ -16,7 +18,7 @@ const CellList: React.FC = () => {
   ))
 
   return (
-    <div>
+    <div className='cell-list'>
       <AddCell forceVisible={cells.length === 0} previousCellId={null} />
       {renderedCells}
     </div>
