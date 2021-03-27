c6b753e3f47f814d7a9f4586bd0fb0349d6f132c
thenatural86 <philip.smalls86@gmail.com>
2021-03-27 13:14:54 -0400

    implementing a show function no op

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -19,12 +19,11 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const { data, order } = state.cells
     const orderedCells = order.map((id) => data[id])
 
-    const cumulativeCode = [
-      `
+    const showFunc = `
       import _React from 'react'
       import _ReactDOM from 'react-dom'
       
-      const show = (value) => {
+      var show = (value) => {
         const root = document.querySelector('#root')
         if(typeof value === 'object'){
           if(value.$$typeof && value.props){
@@ -36,10 +35,19 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
           root.innerHTML = value
         }
       }
-      `,
-    ]
+      `
+
+    const showFuncNoop = 'var show = () => {}'
+
+    const cumulativeCode = []
+
     for (let c of orderedCells) {
       if (c.type === 'code') {
+        if (c.id === cell.id) {
+          cumulativeCode.push(showFunc)
+        } else {
+          cumulativeCode.push(showFuncNoop)
+        }
         cumulativeCode.push(c.content)
       }
       if (c.id === cell.id) {
@@ -48,7 +56,6 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     }
     return cumulativeCode
   })
-  // console.log(cumulativeCode)
 
   useEffect(() => {
     if (!bundle) {
