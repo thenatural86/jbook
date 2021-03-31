8ccd8196df90c16d36a71e14fe51bda69144e65d
thenatural86 <philip.smalls86@gmail.com>
2021-03-30 20:42:41 -0400

    test

--- src/index.tsx
+++ src/index.tsx
@@ -6,16 +6,11 @@ import CellList from './components/cell-list'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'bulmaswatch/superhero/bulmaswatch.min.css'
 
-// import CodeCell from './components/code-cell'
-// import TextEditor from './components/text-editor'
-
 const App = () => {
   return (
     <Provider store={store}>
       <div>
         <CellList />
-        {/* <CodeCell />
-        <TextEditor /> */}
       </div>
     </Provider>
   )
