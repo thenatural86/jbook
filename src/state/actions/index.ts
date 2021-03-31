110d45135055768a612446cedb8093de28ed97ee
thenatural86 <philip.smalls86@gmail.com>
2021-03-31 12:40:10 -0400

    fetch cells logic added to redux

--- src/state/actions/index.ts
+++ src/state/actions/index.ts
@@ -1,5 +1,5 @@
 import { ActionType } from '../action-types'
-import { CellTypes } from '../cell'
+import { Cell, CellTypes } from '../cell'
 export type Direction = 'up' | 'down'
 export interface MoveCellAction {
   type: ActionType.MOVE_CELL
@@ -45,6 +45,18 @@ export interface BundleCompleteAction {
   }
 }
 
+export interface FetchCellsAction {
+  type: ActionType.FETCH_CELLS
+}
+export interface FetchCellsCompleteAction {
+  type: ActionType.FETCH_CELLS_COMPLETE
+  payload: Cell[]
+}
+export interface FetchCellsErrorAction {
+  type: ActionType.FETCH_CELLS_ERROR
+  payload: string
+}
+
 export type Action =
   | MoveCellAction
   | DeleteCellAction
@@ -52,3 +64,6 @@ export type Action =
   | UpdateCellAction
   | BundleStartAction
   | BundleCompleteAction
+  | FetchCellsAction
+  | FetchCellsCompleteAction
+  | FetchCellsErrorAction
